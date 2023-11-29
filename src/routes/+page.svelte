a8e905e7d29966a2d2b337cf1b927a6f0e7667c7
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-11-29 21:21:44 +0100

    layout switch works finally

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,54 +57,10 @@
 	}
 
 	article {
-		width: calc(var(--unit-deca) * 2);
 		display: grid;
 		grid-template-columns: 1fr;
 		margin: auto;
 	}
-
-	/* .searchbar {
-		width: 100%;
-		margin: 0 auto;
-		margin-bottom: 0px;
-		margin-bottom: var(--unit-default);
-		height: auto;
-		display: block;
-		padding: 0;
-	}
-
-	.searchbar form {
-		display: flex;
-		flex-direction: row;
-		gap: var(--unit-small);
-		width: 100%;
-		background-color: var(--color-secundary);
-		border-radius: var(--unit-small);
-		padding: var(--unit-small);
-		position: relative;
-	}
-
-	.searchbar input {
-		width: inherit;
-		border-radius: var(--unit-micro);
-		border: unset;
-		padding-left: var(--unit-small);
-	}
-
-	.searchbar figure {
-		display: flex;
-		align-items: center;
-		gap: var(--unit-small);
-	}
-
-	.searchbar button svg {
-		fill: var(--color-accent-75);
-		transition: var(--animation-default);
-	}
-
-	.searchbar button:is(:hover, :focus) svg {
-		fill: var(--color-blue);
-	} */
 
 	@media (min-width: 42rem) {
 		.grid-overview article {
