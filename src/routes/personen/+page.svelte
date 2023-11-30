8ce1e261c77f8ec011eeb81a5ecc575d0b447fe4
RayanSp <rayan.spall@hva.nl>
2023-11-30 12:05:49 +0100

    .

--- src/routes/personen/+page.svelte
+++ src/routes/personen/+page.svelte
@@ -1,6 +1,8 @@
 <script>
-    // import Persoon from '$lib/components/Personen/persoon.svelte';
+    import Navbar from '$lib/molecules/navigation.svelte';
     import Personen from '$lib/organisms/personen.svelte';
 </script>
+
+<Navbar />
 
 <Personen />
