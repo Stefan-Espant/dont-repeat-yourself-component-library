98ca194b9f8cda7d384dc118976d46171b9abe3a
MokhtarAkle <mokhtar.akle@hva.nl>
2023-11-30 01:38:47 +0100

    Revert "Merge branch 'status'"
    This reverts commit 7a8a56bfcdd911e0f4c5c53010bd61826d4f3514, reversing
    changes made to 0fef7d6a7b7af37d18388753bdc587a439d9b300.

--- src/routes/wens/[id]/+page.server.js
+++ src/routes/wens/[id]/+page.server.js
@@ -1,44 +1,33 @@
 import { gql } from 'graphql-request';
 import { hygraph } from '$lib/utils/hygraph.js';
+import { GraphQLClient } from 'graphql-request';
 
 export async function load({ params }) {
 	const { id } = params;
 
-  let query = gql`
-    query getWish($id: ID!) {
-      wish(where: { id: $id }) {
-        id
-        heading
-        description
-        date
-        label
-        image {
-          url
-        }
-      }
-      statusUpdates {
-        date
-        comment {
-          text
-        }
-        uid
-        id
-      }
-    }
-  `;
+	let query = gql`
+		query getWish($id: ID!) {
+			wish(where: { id: $id }) {
+				id
+				heading
+				description
+				date
+				label
+				image {
+					url
+				}
+			}
+		}
+	`;
 
-
-  const variables = { id };
+	const variables = { id };
 
 	const request = await hygraph.request(query, variables);
 
-  return {
-  
-      wish: request.wish, // Hier halen we de enkele wens op
-      statusupdates: request.statusUpdates
-  };
+	return {
+		wish: request.wish // Hier halen we de enkele wens op
+	};
 }
-
 
 // export async ({ body }, res) => {
 //   const hygraph = new GraphQLClient(
