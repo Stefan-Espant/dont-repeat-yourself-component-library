6d69fec5d22f604de2ecbf844377a70edbbe6661
MokhtarAkle <mokhtar.akle@hva.nl>
2023-11-30 01:43:12 +0100

    Revert "Revert "Merge branch 'status'""
    This reverts commit 98ca194b9f8cda7d384dc118976d46171b9abe3a.

---
+++ src/lib/atoms/statusdatum.svelte
@@ -0,0 +1,18 @@
+<script>
+export let data;
+
+</script>
+<section>
+{#each data.statusupdates as statusupdate}
+
+<p>{statusupdate.date}</p>
+
+		{/each}
+    </section>
+
+<style>
+        p{
+        font-size: 14px;
+        padding-top: 3em;
+    }
+</style>
